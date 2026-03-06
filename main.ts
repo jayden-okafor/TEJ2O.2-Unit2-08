7f26080adb485da6b5ca728fc31cc7afb5ad70df
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-03-06 13:26:18 -0500

    changed code

--- main.ts
+++ main.ts
@@ -13,38 +13,56 @@ basic.showIcon(IconNames.Happy)
 
 // when button "a" is pressed
 input.onButtonPressed(Button.A, function () {
+
+// turn all colours off
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+
+// clear screen
 basic.clearScreen()
+
+// show checkmark
 basic.showIcon(IconNames.Yes)
 
 // turn on red light for 1 second
 pins.digitalWritePin(DigitalPin.P13, 1)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
 basic.pause(1000)
 
 // turn off red and turn on green for 1 second
 pins.digitalWritePin(DigitalPin.P13, 0)
 pins.digitalWritePin(DigitalPin.P14, 1)
+pins.digitalWritePin(DigitalPin.P15, 0)
 basic.pause(1000)
 
 // turn off green and turn on blue for 1 second
+pins.digitalWritePin(DigitalPin.P13, 0)
 pins.digitalWritePin(DigitalPin.P14, 0)
 pins.digitalWritePin(DigitalPin.P15, 1)
 basic.pause(1000)
 
 // turn on red while leaving blue on, which creates magnenta. for 1 second
 pins.digitalWritePin(DigitalPin.P13, 1)
+pins.digitalWritePin(DigitalPin.P15, 1)
 basic.pause(1000)
 
 // turn off red and turn on green while still leaving blue on. which creates cyan. for 1 second
 pins.digitalWritePin(DigitalPin.P13, 0)
 pins.digitalWritePin(DigitalPin.P14, 1)
+pins.digitalWritePin(DigitalPin.P15, 1)
 basic.pause(1000)
 
 // turn on red and turn off blue while leaving green on to create yellow. for 1 second.
 pins.digitalWritePin(DigitalPin.P13, 1)
+pins.digitalWritePin(DigitalPin.P14, 1)
 pins.digitalWritePin(DigitalPin.P15, 0)
 basic.pause(1000)
 
 // turn on blue while leaving red and green still on. which makes white. for 1 second
+pins.digitalWritePin(DigitalPin.P13, 1)
+pins.digitalWritePin(DigitalPin.P14, 1)
 pins.digitalWritePin(DigitalPin.P15, 1)
 basic.pause(1000)
 
